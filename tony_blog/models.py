560828c131b97b917d6a13701a48cd1c0a3ac445
Tony Jafar <Thaer.Jafar@md.de>
2019-11-14 14:57:47 +0100

    update for django > 2.0

--- tony_blog/models.py
+++ tony_blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from taggit.managers import TaggableManager
 
 # Create your models here.
@@ -20,7 +20,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, related_name='blog_posts')
+    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.PROTECT)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -48,7 +48,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name='comments')
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.PROTECT)
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
